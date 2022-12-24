fe97bc562bc3a318702c81bb5e18ffab97634a3a
meankit2610 <ankitsingh5282770@gmail.com>
2022-12-24 11:11:39 +0530

    Home and Header done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Cart from "./components/Cart";
+import Header from "./components/Header";
 import Home from "./components/Home";
 
 import "./styles/app.scss"
 function App() {
   return (
-    <div className="App">
-     <Home/>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/cart" element={<Cart/>} />
+      </Routes>
+        
+    </BrowserRouter>
   );
 }
 
